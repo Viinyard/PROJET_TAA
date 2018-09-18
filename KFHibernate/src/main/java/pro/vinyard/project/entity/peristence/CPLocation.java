5877dbe1121bd5868dbeb950760e4a9a271eaa95
vinyard <antoineleval@gmail.com>
2018-09-18 10:57:08 +0200

    modification des types lng et lat de Location (String vers double), test class de CPLocation fini, success

--- KFHibernate/src/main/java/pro/vinyard/project/entity/peristence/CPLocation.java
+++ KFHibernate/src/main/java/pro/vinyard/project/entity/peristence/CPLocation.java
@@ -12,16 +12,16 @@ public class CPLocation {
 	@GeneratedValue
 	private String id;
 	
-	private String lat;
+	private double lat;
 	
-	private String lng;
+	private double lng;
 	
 	public CPLocation(String latLng) {
 		super();
 		String[] tmp = latLng.split(",");
 		if (tmp.length == 2) {
-			this.setLat(tmp[0]);
-			this.setLng(tmp[1]);
+			this.setLat(Double.parseDouble(tmp[0]));
+			this.setLng(Double.parseDouble(tmp[1]));
 		}
 		
 		if (tmp.length != 2 || !latLng.equals(this.toString())) {
@@ -32,19 +32,19 @@ public class CPLocation {
 	public CPLocation() {
 	}
 	
-	public String getLat() {
+	public double getLat() {
 		return lat;
 	}
 	
-	public void setLat(String lat) {
+	public void setLat(double lat) {
 		this.lat = lat;
 	}
 	
-	public String getLng() {
+	public double getLng() {
 		return lng;
 	}
 	
-	public void setLng(String lng) {
+	public void setLng(double lng) {
 		this.lng = lng;
 	}
 	
